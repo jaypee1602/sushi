2b6dcec50a4558093ce423b8479bd789f15f5960
Ryan Mori <ryan.mori@eonics.nl>
2022-06-21 20:21:05 +0200

    basis website

--- sushi/website/urls.py
+++ sushi/website/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('', views.home, name="home"),
-    path('', views.home, name="home"),
-    path('', views.home, name="home"),
-    path('', views.home, name="home"),
-
+    path('menu', views.menu, name="menu"),
+    path('cart', views.cart, name="cart"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
 ]
--- sushi/website/views.py
+++ sushi/website/views.py
@@ -6,16 +6,16 @@ def home(request):
 
 
 def menu(request):
-    return render(request, 'home.html', {})
-
-
-def home(request):
-    return render(request, 'home.html', {})
-
-
-def home(request):
-    return render(request, 'home.html', {})
+    return render(request, 'menu.html', {})
 
 
 def cart(request):
-    return render(request, 'home.html', {})
+    return render(request, 'cart.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})

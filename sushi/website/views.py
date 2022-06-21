5072f1168fca9fdbdf1089a4cbc0005e626e5675
Ryan Mori <ryan.mori@eonics.nl>
2022-06-21 19:44:04 +0200

    added style

--- sushi/website/views.py
+++ sushi/website/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def menu(request):
+    return render(request, 'menu.html', {})
+
+
+def order(request):
+    return render(request, 'order.html', {})
